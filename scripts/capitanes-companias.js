bf2fe89073c4f94557575211a081978fda02889c
gfmendiburu-git <gfmendiburu@icloud.com>
2026-07-24 21:11:22 +0200

    Integra datos de El Irunes numero 52

--- scripts/capitanes-companias.js
+++ scripts/capitanes-companias.js
@@ -86,7 +86,7 @@
     comandante: "Comandantes documentados",
   };
 
-  fetch("../data/capitanes-companias.json?v=3")
+  fetch("../data/capitanes-companias.json?v=4")
     .then((response) => response.json())
     .then((data) => {
       const entries = (data.entries || [])
