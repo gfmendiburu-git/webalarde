da78bbf9a00fca2fd657060ed555403942f8d691
gfmendiburu-git <gfmendiburu@icloud.com>
2026-07-25 14:19:15 +0200

    Revisa minuciosamente El Irunes numero 11

--- scripts/capitanes-companias.js
+++ scripts/capitanes-companias.js
@@ -86,7 +86,7 @@
     comandante: "Comandantes documentados",
   };
 
-  fetch("../data/capitanes-companias.json?v=7")
+  fetch("../data/capitanes-companias.json?v=8")
     .then((response) => response.json())
     .then((data) => {
       const entries = (data.entries || [])
