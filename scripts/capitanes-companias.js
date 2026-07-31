23c25634abdb3579cab1332b525c99feaf4cd83f
gfmendiburu-git <gfmendiburu@icloud.com>
2026-07-31 12:13:39 +0200

    Integra revision de Pridenoa y El Pueblo Vasco

--- scripts/capitanes-companias.js
+++ scripts/capitanes-companias.js
@@ -86,7 +86,7 @@
     comandante: "Comandantes documentados",
   };
 
-  fetch("../data/capitanes-companias.json?v=12")
+  fetch("../data/capitanes-companias.json?v=13")
     .then((response) => response.json())
     .then((data) => {
       const entries = (data.entries || [])
